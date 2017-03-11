142c56f6313c1d5457a167fa30ebb831784a53f3
Pavel Bezpalov <pavel.bezpalov@cyber-wizard.com>
2017-03-11 18:37:49 +0200

    Add extraction amount from slots

--- app/controllers/alexa_controller.rb
+++ app/controllers/alexa_controller.rb
@@ -8,9 +8,9 @@ class AlexaController < ApplicationController
     p session.has_attributes?
     p session.session_id
     p session.user_defined?
-    Transaction.create(amount: session.amount)
+    Transaction.create(amount: alexa_request.slots['Amount']['value']) if alexa_request.slots.key?('Amount')
     response = AlexaRubykit::Response.new
-    response.add_speech("Hello Everlabs! Your total income is #{Transaction.sum(&:amount).to_i} hrivnas!")
+    response.add_speech("Hello Everlabs! Your total income is #{Transaction.sum(:amount).to_i} hrivnas!")
     response.add_hash_card( { title: 'Everlabs Ready', subtitle: 'Working hard!' })
     render json: response.build_response
   end
