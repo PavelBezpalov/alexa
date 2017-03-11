d648537174912b945baea8e43edb2842e12d6213
Pavel Bezpalov <pavel.bezpalov@cyber-wizard.com>
2017-03-12 01:25:21 +0200

    Add delete_income placeholder

---
+++ app/lib/intents/delete_income.rb
@@ -0,0 +1,23 @@
+module Intents
+  class DeleteIncome
+    attr_reader :user, :request
+    attr_accessor :response
+
+    def initialize(user, request)
+      @user = user
+      @request = request
+      @response = AlexaRubykit::Response.new
+    end
+
+    def call
+      process_request
+    end
+
+    def alexa_response
+      response.build_response
+    end
+
+    def process_request
+    end
+  end
+end
