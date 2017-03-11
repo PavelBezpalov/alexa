6d32ad4c68797876e8a8673325e8531adc7f60e9
Pavel Bezpalov <pavel.bezpalov@cyber-wizard.com>
2017-03-11 19:45:07 +0200

    Add more intents

--- app/controllers/alexa_controller.rb
+++ app/controllers/alexa_controller.rb
@@ -4,14 +4,29 @@ class AlexaController < ApplicationController
     alexa_request = AlexaRubykit.build_request(request_json)
 
     session = alexa_request.session
-    p session.new?
-    p session.has_attributes?
-    p session.session_id
-    p session.user_defined?
-    Transaction.create(amount: alexa_request.slots['Amount']['value']) if alexa_request.slots.key?('Amount')
-    response = AlexaRubykit::Response.new
-    response.add_speech("Hello Everlabs! Your total income is #{Transaction.sum(:amount).to_i} hrivnas!")
-    response.add_hash_card( { title: 'Everlabs Ready', subtitle: 'Working hard!' })
+    logger.info session.new?
+    logger.info session.has_attributes?
+    logger.info session.session_id
+    logger.info session.user_defined?
+
+    if (alexa_request.type == 'INTENT_REQUEST')
+      logger.info "#{request.slots}"
+      logger.info "#{request.name}"
+      Transaction.create(amount: alexa_request.slots['Amount']['value']) if alexa_request.slots.key?('Amount')
+      response = AlexaRubykit::Response.new
+      response.add_speech("Your total income is #{Transaction.sum(:amount).to_i} hrivnas!")
+    end
+
+    if (alexa_request.type == 'LAUNCH_REQUEST')
+      response.add_speech('FOP reeeeeeaaaaaaaaaady!')
+    end
+
+    if (alexa_request.type =='SESSION_ENDED_REQUEST')
+      logger.info "#{request.type}"
+      logger.info "#{request.reason}"
+      head 200 and return
+    end
+
     render json: response.build_response
   end
 end
