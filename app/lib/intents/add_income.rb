cb916bbc9e30acf437d84256b9e49694426e9cc3
Oleg Pasko <olegpasko@gmail.com>
2017-03-12 10:27:47 +0200

    date for the past

--- app/lib/intents/add_income.rb
+++ app/lib/intents/add_income.rb
@@ -18,7 +18,7 @@ module Intents
     end
 
     def process_request
-      transaction_date = Date.parse(request.slots['Date']['value'] || Time.zone.today.to_s)
+      transaction_date = Date.parse(request.slots['Date']['value'].gsub('2018','2017') || Time.zone.today.to_s)
       transaction = Transaction.new(amount: request.slots['Amount']['value'], transaction_date: transaction_date,
                                     user: user)
 
