fbbc1641134e2e270319ad650caac64339f552f0
Oleg Pasko <olegpasko@gmail.com>
2017-03-12 10:40:37 +0200

    add income date gsub

--- app/lib/intents/add_income.rb
+++ app/lib/intents/add_income.rb
@@ -18,6 +18,7 @@ module Intents
     end
 
     def process_request
+      request.slots['Date']['value'].gsub('2018', '2017') if request.slots['Date']['value'].present?
       transaction_date = Date.parse(request.slots['Date']['value'] || Time.zone.today.to_s)
       transaction = Transaction.new(amount: request.slots['Amount']['value'], transaction_date: transaction_date,
                                     user: user)
